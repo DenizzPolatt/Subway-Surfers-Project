fbbeb1c67d8d108e8ab6c696d3130959b99c3787
Denlz Polat <91384872+DenizzPolatt@users.noreply.github.com>
2023-01-12 17:23:44 +0300

    Made Player Run by Default

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Move()
+    {
+        
+    }
+}
